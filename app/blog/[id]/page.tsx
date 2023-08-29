6fb1a89417afbf3f2bfda11e9f7eeafaaef0aa6c
linzhe141 <linzhe141@qq.com>
2023-08-29 23:22:42 +0800

    [feat]

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -2,9 +2,11 @@
 import Hello from './hello.mdx'
 export default function Test({ params }: { params: { id: string } }) {
   return (
-    <div className='prose w-full'>
-      <Hello />
+    <div className='prose max-w-full'>
       <div>My Post: {params.id}</div>
+      {/* <div>test __dirname: {__dirname}</div> */}
+      {/* <div>test __filename: {__filename}</div> */}
+      <Hello />
     </div>
   )
 }
