03cc43875ebc310e40bf345f53e457fd66479844
linzhe <linzhe141@qq.com>
2023-08-29 18:00:56 +0800

    [feat] 新增mdx实例

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,6 +1,9 @@
+'use client'
+import Hello from './hello.mdx'
 export default function Test({ params }: { params: { id: string } }) {
   return (
-    <div>
+    <div className='prose w-full'>
+      <Hello />
       <div>My Post: {params.id}</div>
     </div>
   )
